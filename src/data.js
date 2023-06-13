b9eff6764b36b77df909e6acac549ed8a62fa290
Zionnaire <96918896+Zionnaire@users.noreply.github.com>
2023-06-13 04:13:46 -0700

    Multimedia-app

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Sample video',
+      type: 'video',
+      path: "/file-server/Sample video.mp4"
+    },
+    {
+      id: 2,
+      name: 'Spider video',
+      type: 'video',
+      path: "/file-server/Spider video.mp4"
+    },
+    {
+      id: 3,
+      name: 'Sample sound',
+      type: 'audio',
+      path: "/file-server/Sample sound.mp3"
+    },
+    {
+      id: 4,
+      name: 'Sample pdf',
+      type: 'document',
+      path: "/file-server/Sample pdf.pdf"
+    },
+    {
+      id: 5,
+      name: 'Spiderman',
+      type: 'image',
+      path: "/file-server/Spiderman.png"
+    },
+    {
+        id: 6,
+        name: 'batman',
+        type: 'image',
+        path: "/file-server/batman.png"
+      },
+      {
+        id: 7,
+        name: 'joker',
+        type: 'image',
+        path: "/file-server/joker.png"
+      },
+      {
+        id: 8,
+        name: 'Hulk',
+        type: 'image',
+        path: "/file-server/Hulk.png"
+      },
+  ]
